4868cf4cf0ed62d728df0215bb11fd773206a961
Henrique Marinho <henriquemarinho11@hotmail.com>
2020-04-02 18:12:37 -0300

    AC02_Primos_H

---
+++ AC02_Primos.py
@@ -0,0 +1,20 @@
+def primo (n):
+    if n != 0 and n !=1:
+        if n>3:
+            for i in range(2,n):
+                if n %i==0:
+                    return False
+        return True
+    return
+
+
+i=1
+cont =0
+while True:
+    primo(i)
+    i+=1
+    if primo(i) == True:
+        cont+=1
+        print(i)
+    if cont == 100:
+        break
